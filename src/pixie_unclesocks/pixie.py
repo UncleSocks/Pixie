779cd1558fcc0a74d0b003423da5967a02241f4a
UncleSocks <ilisan.tyrone@gmail.com>
2025-05-12 10:19:35 +0800

    Update: Added BLACKLISTED filter and cleaned up directories.

--- src/pixie_unclesocks/pixie.py
+++ src/pixie_unclesocks/pixie.py
@@ -160,8 +160,8 @@ class AbuseIpDbLookup():
                 domain = "NOT APPLICABLE"
                 isp = "NOT APPLICABLE"
                 abuse_raw_score = 0
-                abuse_score = "NOT APPLICABLE"
-                total_reports = "NOT APPLICABLE"
+                abuse_score = 0
+                total_reports = 0
                 last_reported_at = "NOT APPLICABLE"
 
             processed_ip = {"IP Address":str(ip_address), "Country Code":str(country_code), "Hostnames":str(hostnames), "Domain":str(domain), "ISP":str(isp), "Raw Abuse Score":int(abuse_raw_score), \
@@ -276,19 +276,34 @@ class FilterLogic():
             "COUNTRY": {
                 "extract": lambda record: record.get('Country Code', '').upper(),
                 "cast": str
+            },
+
+            "BLACKLISTED": {
+                "extract": lambda record: record.get('Blacklisted', False),
+                "cast": self._bool_cast
             }
         }
         
+
     def _contains(self, a_string, b_string):
         return b_string.upper() in a_string.upper()
     
     def _not_contains(self, a_string, b_string):
         return b_string.upper() not in a_string.upper()
-    
+
+    def _bool_cast(self, value):
+
+        if value.strip().upper() in ("TRUE", "YES", "1"):
+            return True
+        elif value.strip().upper() in ("FALSE", "NO", "0"):
+            return False
+        else:
+            raise ValueError(f"ERR-FL05: Invalid value {value} for BLACKLISTED field. Use True/False, Yes/No, or 1/0 only.")
+
 
     def build_filter(self):
         
-        filter_pattern = re.compile(r'((((?P<filter_key_int>CONFIDENCE|TOTALREPORTS)(?:\s*)?(?P<filter_op_int>>=|<=|==|!=|>|<|=))|((?P<filter_key_str>ISP|COUNTRY)(?:\s*)?(?P<filter_op_str>contains|!contains)))(?:\s*)?(?P<filter_value>\S+))', 
+        filter_pattern = re.compile(r'((((?P<filter_key_int>CONFIDENCE|TOTALREPORTS)(?:\s*)?(?P<filter_op_int>>=|<=|==|!=|>|<|=))|((?P<filter_key_str>ISP|COUNTRY)(?:\s*)?(?P<filter_op_str>contains|!contains))|((?P<filter_key_bl>BLACKLISTED)(?:\s*)?(?P<filter_op_bl>==|=)))(?:\s*)?(?P<filter_value>\S+))', 
                                     re.IGNORECASE)
         if not filter_pattern:
             raise ValueError(f"Invalid filter format {filter}")
@@ -298,26 +313,28 @@ class FilterLogic():
         for filter in self.filter_args:
             filter_match = filter_pattern.fullmatch(filter)
             if not filter_match:
-                raise ValueError(f"ERR-FL01: Invalid filter format: '{filter}'. Expected format like 'CONFIDENCE>=85'.")            
+                raise ValueError(f"ERR-FL01: Invalid filter format: '{filter}'. Expected format like 'CONFIDENCE >= 85'.")            
 
-            key = filter_match.group('filter_key_int') or filter_match.group('filter_key_str')
-            op = filter_match.group('filter_op_int') or filter_match.group('filter_op_str')
+            key = filter_match.group('filter_key_int') or filter_match.group('filter_key_str') or filter_match.group('filter_key_bl')
+            op = filter_match.group('filter_op_int') or filter_match.group('filter_op_str') or filter_match.group('filter_op_bl')
             value = filter_match.group('filter_value')
 
-            config = self.filter_config[key]
+            config = self.filter_config.get(key)
+            if not config:
+                raise ValueError(f"ERR-FL02: Unknown filter key {key}.")
+
             extracted = config['extract']
             cast = config['cast']
 
             try:
                 op_func = self.operator_map[op]
             except:
-                raise ValueError(f"ERR-FL02: Invalid operator. Use --help option for more information on the available operators.")
+                raise ValueError(f"ERR-FL03: Invalid operator. Use --help option for more information on the available operators.")
 
             try:
                 value = cast(value)
             except:
-               raise ValueError(f"ERR-FL03: Invalid {value} cast value. Cast must be {cast}.") 
-            
+               raise ValueError(f"ERR-FL04: Invalid cast for value {value}.") 
 
             parsed_filters.append(lambda record, op=op_func, value=value, extracted=extracted: op(extracted(record), value))
 
@@ -352,6 +369,9 @@ class DisplayOutput():
             table.add_row([ip['IP Address'], ip['Country Code'], ip['Hostnames'], ip['Domain'], ip['ISP'], ip['Abuse Score'], ip['Total Reports'], ip['Last Reported At'], ip['Blacklisted']])
 
         table.align = "l"
+        table.max_width['Hostnames'] = 20
+        table.max_width['Domain'] = 20
+        table.max_width['ISP'] = 20
 
         return print(table)
     
